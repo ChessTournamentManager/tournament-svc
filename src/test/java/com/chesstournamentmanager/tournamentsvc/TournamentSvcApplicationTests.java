8d21f699a54840fd983d0620e433b358d549f742
Judah <i460958@fhict.nl>
2022-09-20 22:31:17 +0200

    Added a test

--- src/test/java/com/chesstournamentmanager/tournamentsvc/TournamentSvcApplicationTests.java
+++ src/test/java/com/chesstournamentmanager/tournamentsvc/TournamentSvcApplicationTests.java
@@ -1,13 +1,22 @@
 package com.chesstournamentmanager.tournamentsvc;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TournamentSvcApplicationTests {
 
+	@Autowired
+	private TournamentSvcApplication tournamentSvcApplication;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sendsWelcomeMessage() {
+		assertThat(tournamentSvcApplication.getMessage()).isEqualTo("Welcome to the tournament service.");
+	}
 }
